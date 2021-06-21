99e328302741314727eece76fb0ccb73740a6df0
Nico <nico.03727+github@gmail.com>
2021-06-20 23:00:32 -0400

    initial commit

---
+++ scripts/setup.js
@@ -0,0 +1,15 @@
+const { exec } = require('child_process');
+const logExecOutput = data => console.log(data);
+
+const runNPMInstall = exec('cd .. && npm i');
+runNPMInstall.stdout.on('data', logExecOutput);
+runNPMInstall.stderr.on('data', logExecOutput);
+runNPMInstall.on('close', () => {
+    const runBuild = exec('cd .. && npm run build');
+    runBuild.stdout.on('data', logExecOutput);
+    runBuild.stderr.on('data', logExecOutput);
+    runBuild.on('close', () => {
+        console.log('Setup complete.');
+        process.exit();
+    });
+});
